fcfb3ccc8791d751ec1018adc0e9e98ea8363ebd
JeanCarloH <jecherrerade@unal.edu.co>
2022-09-17 21:57:37 -0500

    configurando

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import logo from "./logo.svg";
 import "./App.css";
-import Button from '@mui/material/Button';
 import { AuthProvider } from "./context/authContext";
 import Login from "./components/Login";
 import Home from "./components/Home";
@@ -20,50 +15,60 @@ import AddUserNote from "./components/AddUserNote";
 import UserPayment from "./components/UserPayment";
 import UserReady from "./components/UserReady";
 import UserStatistics from "./components/UserStatistics";
+import {xd} from "./components/types";
 import { getToken2 } from "./firebase";
 import { onMessageListener } from "./firebase";
-import { xd } from "./components/types";
+import Button from "@mui/material/Button";
+import { IconButton, Snackbar } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 function App() {
-  // if ('serviceWorker' in navigator) {
-  //   window.addEventListener('load', function() {
-  //     navigator.serviceWorker.register('/sw.js').then(function(registration) {
-  //       // Si es exitoso
-  //       console.log('SW registrado correctamente');
-  //     }, function(err) {
-  //       // Si falla
-  //       console.log('SW fallo', err);
-  //     });
-  //   });
-  // }
-  //const [openNotificacion, setOpenNotificacion] = React.useState(false);
-
   const [show, setShow] = useState(false);
-  const [notification, setNotification] = useState<xd>({title: '', body: ''});
-
   const [isTokenFound, setTokenFound] = useState(false);
-  getToken2(setTokenFound);
-
+  const [notification, setNotification] = useState<xd>({title: '', body: ''});
+    getToken2(setTokenFound);
+ 
+    onMessageListener().then((payload:any)=> {
+      setShow(true);
+      setOpen(true);
+      setNotification({title: payload.notification.title, body: payload.notification.body})
+      console.log(payload);
+    }).catch(err => console.log('failed: ', err));
   const [state, dispatch] = useReducer(userReducer, userInitialState);
+  const [open, setOpen] = React.useState(false);
 
-  onMessageListener().then((payload:any) => {
-    setShow(true);
-    setNotification({title: payload.notification.title, body: payload.notification.body})
-    console.log(payload);
-  }).catch(err => console.log('failed: ', err));
-  const handleClose = () => {
-    setShow(false);
-   
+ 
+
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
   };
+
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
   return (
     <>
-      <AuthProvider >
+      <AuthProvider>
         <HashRouter>
           <Routes>
             <Route path="/" element={<Login />} />
             
             
-           
-
            
             <Route
               path="/Admin"
@@ -89,24 +94,17 @@ function App() {
           </Routes>
         </HashRouter>
       </AuthProvider>
-   {show ?(
-         <Dialog open={show} onClose={handleClose}>
-         <DialogTitle>{notification.title}</DialogTitle>
-         <DialogContent>
-         {notification.body}
-         </DialogContent>
-         <DialogActions>
-           <Button onClick={handleClose}>Cancelar</Button>
-        
-           <Button onClick={handleClose}>Aceptar</Button>
-         </DialogActions>
-       </Dialog>
-   ):(
-      <></>
-   )}
+      <div>
+      
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        message={notification.title}
+        action={action}
+      />
+    </div>
 
-
-   
     </>
   );
 }
